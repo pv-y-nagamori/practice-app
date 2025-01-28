a0b919222ce10a95d961659de645dd3b895be806
pv-y-nagamori <y-nagamori@pro-vision.jp>
2025-01-28 14:03:52 +0900

    ログイン画面

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,33 +1,40 @@
 "use client"
 import React from 'react';
 import { signIn } from 'next-auth/react';
-import { Button } from '@mui/material';
 import { styled } from '@mui/system';
+import GoogleIcon from '@mui/icons-material/Google';
+import { Button, Typography } from '@mui/material';
 
 
 
 export default function Login() {
 
 	const Container = styled('div')({
+		display: 'flex',
+		flexDirection: 'column',
 		alignItems: 'center',
 		justifyContent: 'center',
+		height: '100vh',
 	  });
 	  
 	  const LoginCard = styled('div')({
 		backgroundColor: '#fff',
-		padding: '100px',
+		padding: '20px',
 		borderRadius: '10px',
 		boxShadow: '0 0 10px rgba(0,0,0,0.1)',
 	  });
 
 	return (
-		<Container>
-		  <LoginCard>
+	<Container>
+		<LoginCard>
+			<Typography variant="h5" gutterBottom>
+			sign in with
+			</Typography>
 			<Button variant="contained" color="primary" fullWidth onClick={() => signIn('google', {}, { prompt: 'login' })}>
-			  ログイン
+			  <GoogleIcon /> google Login
 			</Button>
-		  </LoginCard>
-		</Container>
+		</LoginCard>
+	</Container>
 	);
 
 }
